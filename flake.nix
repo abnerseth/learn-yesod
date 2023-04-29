722ffd101b76a0f3799f2f5d636cbecc70183ace
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-04-28 21:30:18 -0400

    Replace haskell.nix flake with simpler shellFor template

--- flake.nix
+++ flake.nix
@@ -1,42 +1,40 @@
 {
-  description = "Learn Yesod";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  description = "learn-yesod";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+
+  outputs = inputs:
     let
-      overlays = [ haskellNix.overlay
-        (final: prev: {
-          # This overlay adds our project to pkgs
-          learnYesodProject =
-            final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc925";
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = {};
-                hlint = {};
-                haskell-language-server = {};
-              };
-              # Non-Haskell shell tools go here
-              shell.buildInputs = with pkgs; [
-                nixpkgs-fmt
-                zlib
-              ];
-              # This adds `js-unknown-ghcjs-cabal` to the shell.
-              # shell.crossPlatforms = p: [p.ghcjs];
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              learnYesod = hfinal.callCabal2nix "learn-yesod" ./. { };
             };
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-      flake = pkgs.learnYesodProject.flake {
-        # This adds support for `nix build .#js-unknown-ghcjs:hello:exe:hello`
-        # crossPlatforms = p: [p.ghcjs];
+        };
+        PKGNAME = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.learnYesod;
       };
-    in flake // {
-      # Built by `nix build .`
-      packages.default = flake.packages."learn-yesod:exe:learn-yesod";
-    });
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+        in
+        {
+          devShell = hspkgs.shellFor {
+            withHoogle = true;
+            packages = p: [ p.learnYesod ];
+            buildInputs = [
+              hspkgs.cabal-install
+              hspkgs.haskell-language-server
+              hspkgs.hlint
+              hspkgs.ormolu
+              pkgs.bashInteractive
+              pkgs.zlib
+            ];
+          };
+          defaultPackage = pkgs.learnYesod;
+        };
+    in
+    { inherit overlay; } // inputs.flake-utils.lib.eachDefaultSystem perSystem;
 }
