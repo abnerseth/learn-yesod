9e22058a8a9e4b752e1c67e3a6c16d64594d1129
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-05-02 16:21:14 -0400

    Misc. config

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,23 @@
 module Main where
 
-import Network.HTTP.Types       (status200)
-import Network.Wai              (Request, Response, ResponseReceived, responseLBS)
+import Network.HTTP.Types (status200)
+import Network.Wai (Request, Response, ResponseReceived, responseLBS)
 import Network.Wai.Handler.Warp (run)
 
 -- type Application = Request
 --                 -> (Response -> IO ResponseReceived)
 --                 -> IO ResponseReceived
 
-app :: Request
-    -> (Response -> IO ResponseReceived)
-    -> IO ResponseReceived
-app _request sendResponse = sendResponse $ responseLBS
-    status200
-    [("Content-Type", "text/plain")]
-    "Hello Warp!"
+app ::
+  Request ->
+  (Response -> IO ResponseReceived) ->
+  IO ResponseReceived
+app _request sendResponse =
+  sendResponse $
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")]
+      "Hello Warp!"
 
 main :: IO ()
 main = run 3000 app
